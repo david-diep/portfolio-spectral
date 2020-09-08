0366d266840b11b968d74fa229786d8bb876a00f
david-diep <ddkotan@gmail.com>
2020-09-07 20:30:32 -0700

    first draft

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+//personal stuff
+var timedSecond = setInterval(writeSecondHeader, 2000);
+setInfo();
+
+function writeSecondHeader() {
+	$("#write").append("<h2 class='text-right m-auto white-cursor'>Welcome to my Website!</h2>");
+	$("#first").removeClass("white-cursor");
+	clearInterval(timedSecond);
+
+}
+
+function setInfo() {
+	$("#email-1").append("<span class='font-weight-bold'>Email: </span> <a href='mailto: ddkotan@gmail.com'>ddkotan@gmail.com</a>")
+	$("#email-2").append("<span class='font-weight-bold'>Email: </span> <a href='mailto: ddkotan@gmail.com'>ddkotan@gmail.com</a>")
+	$("#phone-1").append('<span class="font-weight-bold">Phone: </span> <a href="tel:714-890-2704">(714) 890-2704</a>')
+	$("#phone-2").append('<span class="font-weight-bold">Phone: </span> <a href="tel:714-890-2704">(714) 890-2704</a>')
+}
+
 /*
 	Spectral by HTML5 UP
 	html5up.net | @ajlkn
